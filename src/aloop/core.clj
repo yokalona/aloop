ea635a10abed2494bbcdf2629b48566514cf176d
Roman Makhlin <roman.v.makhlin@gmail.com>
2023-06-01 04:36:35 +0200

    aloop

--- src/aloop/core.clj
+++ src/aloop/core.clj
@@ -4,11 +4,6 @@
   block
   !!!
   example
-  rotate
-  add->>seq
-  add->seq
-  replace->>seq
-  replace->seq
   if-
   if->
   if-|>
@@ -28,7 +23,13 @@
   <<->
   <<->>
   |>
-  <|)
+  <|
+  rotate
+  add->>seq
+  add->seq
+  replace->>seq
+  replace->seq
+  swap)
 
 (defn- -natural->
   [x form]
@@ -50,122 +51,6 @@
 (defmacro block
   [& forms]
   `(comment ~@forms))
-
-(block "Index manipulations")
-
-(defn rotate
-  "Takes `index` and right border `c`. Envelops `index` around the length of `c`, if `index` is positive - envelopes
-  clockwise, otherwise - counterclockwise. In other words, uses different approaches for indexing a collection of
-  length c. For positive values of `index` - count a first element of collection as 0, second as 1 and so on.
-  For negative values - last element of collection will be 0, second to last - 1."
-  [c index]
-  (if (neg? index)
-    (recur c (+ c index))
-    (if+ (>= index c)
-         (recur c (mod index c)))))
-
-(example
-  (rotate 3 0) :=> 0
-  (rotate 3 4) :=> 1
-  (rotate 3 -1) :=> 2)
-
-(block "Functions on seq")
-(!!! "Following functions will use rotation on indices, take a look on:" #'rotate)
-
-(defn add->>seq
-  "Takes an arbitrary collection `where` and an element `what`. Adds `what` to the index `place` in `where`.
-  If no `place` provided - adds `what` to the end of `where`. Negatively rotates index `place`, i.e. counts indices
-  backwards, where last element is 0, second to last is 1 and so on."
-  ([where what]
-   (if+ (seq? where)
-        (->> where
-             reverse
-             (cons what)
-             reverse)))
-  ([where what place]
-   (add->seq where what (- (count where) place))))
-
-(example
-  (add->>seq '(1 2 3 4) 5) :=> '(1 2 3 4 5)
-  (add->>seq '(1 2 3 4) 5 0) :=> '(1 2 3 4 5)
-  (add->>seq '(1 2 3 4) 5 1) :=> '(1 2 3 5 4)
-  (add->>seq '(1 2 3 4) 5 -1) :=> '(5 1 2 3 4))
-
-(defn add->seq
-  "Takes an arbitrary collection `where` and an element `what`. Adds `what` to the index `place` in `where`.
-  If no `place` provided - adds `what` to the beginning of `where`. Positively rotates index `place`,
-  i.e. counts indices forward, where first element is 0, second is 1 and so on."
-  ([where what]
-   (add->seq where what 0))
-  ([where what place]
-   (if+ (seq? where)
-        (let [place (-> where count inc (rotate place))]
-          (->> where
-               (drop place)
-               (concat (add->>seq (take place where) what)))))))
-
-(example
-  (add->seq '(1 2 3 4) 5) :=> '(5 1 2 3 4)
-  (add->seq '(1 2 3 4) 5 3) :=> '(1 2 3 5 4)
-  (add->seq '(1 2 3 4) 5 0) :=> '(5 1 2 3 4)
-  (add->seq '(1 2 3 4) 5 -1) :=> '(1 2 3 4 5))
-
-(defn replace->>seq
-  "Takes an arbitrary collection `where` and an element `what`. Replaces `what` on the index `place` in `where`.
-  If no `place` provided - changes nothing. Negatively rotates index `place`, i.e. counts indices backwards,
-  where last element is 0, second to last is 1 and so on."
-  ([where what]
-   (if+ (seq? where)
-        (if- (empty? where)
-             (-> where
-                 (drop-last)
-                 (add->>seq what)))))
-  ([where what place]
-   (replace->seq where what (-> where count (- place 1)))))
-
-(example
-  (replace->>seq '(1 2 3) 4) :=> '(1 2 4)
-  (replace->>seq '(1 2 3) 4 0) :=> '(1 2 4)
-  (replace->>seq '(1 2 3) 4 1) :=> '(1 4 3)
-  (replace->>seq '(1 2 3) 4 -1) :=> '(4 2 3))
-
-(defn replace->seq
-  "Takes an arbitrary collection `where` and an element `what`. Replaces `what` on the index `place` in `where`.
-  If no `place` provided - changes nothing. Positively rotates index `place`, i.e. counts indices forward, where
-  first element is 0, second is 1 and so on."
-  ([where what]
-   (replace->seq where what 0))
-  ([where what place]
-   (if+ (seq? where)
-        (let [place (-> where count (rotate place))]
-          (-> place
-              (take where)
-              (concat (drop (inc place) where))
-              (add->seq what place))))))
-
-(example
-  (replace->seq '(1 2 3) 4) :=> '(1 2 4)
-  (replace->seq '(1 2 3) 4 0) :=> '(4 2 3)
-  (replace->seq '(1 2 3) 4 -1) :=> '(1 2 4))
-
-(defn swap
-  "Takes an arbitrary collection `where` and two indices: `left` and `right`. Swaps value on the `left` with the value
-  on the `right`. Positively rotates both indices `place`, i.e. counts indices forward, where first element is 0, second
-  is 1 and so on."
-  [where left right]
-  (if+ (seq? where)
-       (if- (empty? where)
-            (let [left-value (nth where (rotate (count where) left))
-                  right-value (nth where (rotate (count where) right))]
-              (-> where
-                  (replace->seq right-value left)
-                  (replace->seq left-value right))))))
-
-(example
-  (swap '(1 2 3 4 5) 0 0) :=> '(1 2 3 4 5)
-  (swap '(1 2 3 4 5) 0 3) :=> '(4 2 3 1 5)
-  (swap '(1 2 3 4 5) 0 -1) :=> '(5 2 3 4 1)
-  (swap '(1 2 3 4 5) -1 -2) :=> '(1 2 3 5 4))
 
 (block "`If` macros")
 
@@ -272,7 +157,9 @@
    executes `then` forms in the implicit `do`"
   [p & then]
   (if-let [arg (second p)]
-    (list 'if p (cons 'do then) arg)))
+    (list 'if p (if (= 1 (count then))
+                  (first then)
+                  (cons 'do then)) arg)))
 
 (example
   (if+ (keyword? :bar)
@@ -440,7 +327,7 @@
                    (if `(get-in (meta (var ~(first form))) [::aloop ::before-thread?])
                      (let [[op form] form]
                        `(~op ~(-natural->> x form)))
-                     (-natural->> x form))]
+                     (-natural->> x form)))]
         (recur (concat d (list with)) (next forms)))
       d)))
 
@@ -509,7 +396,7 @@
   > :boo :foo
   :=> nil)
 
-(block "Threading macros, useful with " #'<-> #'<<-> #'<->> #<<->>)
+(block "Threading macros, useful with " #'<-> #'<<-> #'<->> #'<<->>)
 
 (defmacro |>
   "Same as `->`, but if stumbles across any macro marked as :before-thread? - then firstly executes its form and
@@ -552,3 +439,118 @@
   (<| 5 dec str keyword (<->> (println :1 :2 :3 :5)))
   > :1 :2 :3 :4 :5
   :=> nil)
+
+(block "Index manipulations")
+
+(defn rotate
+  "Takes `index` and right border `c`. Envelops `index` around the length of `c`, if `index` is positive - envelopes
+  clockwise, otherwise - counterclockwise. In other words, uses different approaches for indexing a collection of
+  length c. For positive values of `index` - count a first element of collection as 0, second as 1 and so on.
+  For negative values - last element of collection will be 0, second to last - 1."
+  [c index]
+  (if (neg? index)
+    (recur c (+ c index))
+    (if+ (>= index c)
+         (recur c (mod index c)))))
+
+(example
+  (rotate 3 0) :=> 0
+  (rotate 3 4) :=> 1
+  (rotate 3 -1) :=> 2)
+(block "Functions on seq")
+(!!! "Following functions will use rotation on indices, take a look on:" #'rotate)
+
+(defn add->>seq
+  "Takes an arbitrary collection `where` and an element `what`. Adds `what` to the index `place` in `where`.
+  If no `place` provided - adds `what` to the end of `where`. Negatively rotates index `place`, i.e. counts indices
+  backwards, where last element is 0, second to last is 1 and so on."
+  ([where what]
+   (if+ (seq? where)
+        (->> where
+             reverse
+             (cons what)
+             reverse)))
+  ([where what place]
+   (add->seq where what (- (count where) place))))
+
+(example
+  (add->>seq '(1 2 3 4) 5) :=> '(1 2 3 4 5)
+  (add->>seq '(1 2 3 4) 5 0) :=> '(1 2 3 4 5)
+  (add->>seq '(1 2 3 4) 5 1) :=> '(1 2 3 5 4)
+  (add->>seq '(1 2 3 4) 5 -1) :=> '(5 1 2 3 4))
+
+(defn add->seq
+  "Takes an arbitrary collection `where` and an element `what`. Adds `what` to the index `place` in `where`.
+  If no `place` provided - adds `what` to the beginning of `where`. Positively rotates index `place`,
+  i.e. counts indices forward, where first element is 0, second is 1 and so on."
+  ([where what]
+   (add->seq where what 0))
+  ([where what place]
+   (if+ (seq? where)
+        (let [place (-> where count inc (rotate place))]
+          (->> where
+               (drop place)
+               (concat (add->>seq (take place where) what)))))))
+
+(example
+  (add->seq '(1 2 3 4) 5) :=> '(5 1 2 3 4)
+  (add->seq '(1 2 3 4) 5 3) :=> '(1 2 3 5 4)
+  (add->seq '(1 2 3 4) 5 0) :=> '(5 1 2 3 4)
+  (add->seq '(1 2 3 4) 5 -1) :=> '(1 2 3 4 5))
+
+(defn replace->>seq
+  "Takes an arbitrary collection `where` and an element `what`. Replaces `what` on the index `place` in `where`.
+  If no `place` provided - changes nothing. Negatively rotates index `place`, i.e. counts indices backwards,
+  where last element is 0, second to last is 1 and so on."
+  ([where what]
+   (if+ (seq? where)
+        (if- (empty? where)
+             (-> where
+                 (drop-last)
+                 (add->>seq what)))))
+  ([where what place]
+   (replace->seq where what (-> where count (- place 1)))))
+
+(example
+  (replace->>seq '(1 2 3) 4) :=> '(1 2 4)
+  (replace->>seq '(1 2 3) 4 0) :=> '(1 2 4)
+  (replace->>seq '(1 2 3) 4 1) :=> '(1 4 3)
+  (replace->>seq '(1 2 3) 4 -1) :=> '(4 2 3))
+
+(defn replace->seq
+  "Takes an arbitrary collection `where` and an element `what`. Replaces `what` on the index `place` in `where`.
+  If no `place` provided - changes nothing. Positively rotates index `place`, i.e. counts indices forward, where
+  first element is 0, second is 1 and so on."
+  ([where what]
+   (replace->seq where what 0))
+  ([where what place]
+   (if+ (seq? where)
+        (let [place (-> where count (rotate place))]
+          (-> place
+              (take where)
+              (concat (drop (inc place) where))
+              (add->seq what place))))))
+
+(example
+  (replace->seq '(1 2 3) 4) :=> '(1 2 4)
+  (replace->seq '(1 2 3) 4 0) :=> '(4 2 3)
+  (replace->seq '(1 2 3) 4 -1) :=> '(1 2 4))
+
+(defn swap
+  "Takes an arbitrary collection `where` and two indices: `left` and `right`. Swaps value on the `left` with the value
+  on the `right`. Positively rotates both indices `place`, i.e. counts indices forward, where first element is 0, second
+  is 1 and so on."
+  [where left right]
+  (if+ (seq? where)
+       (if- (empty? where)
+            (let [left-value (nth where (rotate (count where) left))
+                  right-value (nth where (rotate (count where) right))]
+              (-> where
+                  (replace->seq right-value left)
+                  (replace->seq left-value right))))))
+
+(example
+  (swap '(1 2 3 4 5) 0 0) :=> '(1 2 3 4 5)
+  (swap '(1 2 3 4 5) 0 3) :=> '(4 2 3 1 5)
+  (swap '(1 2 3 4 5) 0 -1) :=> '(5 2 3 4 1)
+  (swap '(1 2 3 4 5) -1 -2) :=> '(1 2 3 5 4))
